d7f0a31375c52d5bb29759c4fed5080bd748dac2
Daniel Gruszczynski <dangruszczynski@gmail.com>
2014-08-12 13:39:42 -0400

    Added amazon project -- scaffolded Products

---
+++ amazon/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Amazon::Application.config.secret_key_base = '962dcef934a7c90b502dd2cde2ed24e1cad3af7a65cd0e8c241bd799d4b13bf701f08d4aabbcf12d5808df4c9e48428ef573159aa6867d2bc58c83f12b59b219'
