7732389cd0240eda4ca450c699af0218f2a9409a
Daniel Gruszczynski <dangruszczynski@gmail.com>
2014-08-13 11:04:53 -0400

    Catalog page added and page layout created

--- amazon/app/models/product.rb
+++ amazon/app/models/product.rb
@@ -8,4 +8,10 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for gif, jpg, or png image.'
 	}
+
+	validates :title, length: {minimum: 2}
+
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
